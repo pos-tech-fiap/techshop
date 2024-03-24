96d0fc4ac9bd891de977889a4ca2ec99711446bd
Thcamposmartins <thcampos_@live.com>
2024-03-24 19:10:29 -0300

    first commit ༼ つ ◕_◕ ༽つ

---
+++ ms_user/src/main/java/com/techshopmsuser/repository/IUserRepository.java
@@ -0,0 +1,9 @@
+package com.techshopmsuser.repository;
+
+import com.techshopmsuser.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IUserRepository extends JpaRepository<User, Long> {
+}
